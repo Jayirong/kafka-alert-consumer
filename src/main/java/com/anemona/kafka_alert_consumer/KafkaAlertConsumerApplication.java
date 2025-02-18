051b18caadb8ec462d91c7bf293b157bae034354
Jayirong <lemonsoft5555@gmail.com>
2025-02-17 22:47:26 -0300

    ;au

---
+++ src/main/java/com/anemona/kafka_alert_consumer/KafkaAlertConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.anemona.kafka_alert_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAlertConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAlertConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anemona/kafka_alert_consumer/KafkaAlertConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anemona.kafka_alert_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAlertConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
